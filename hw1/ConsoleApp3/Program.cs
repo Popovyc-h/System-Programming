0b7c62a0c440c69480a8eb8ff14e4e46af201258
Ivan <popovichtanja86@gmail.com>
2026-04-06 01:54:05 +0300

    init

---
+++ hw1/ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp3;
+
+using System.Diagnostics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ProcessStartInfo info = new ProcessStartInfo();
+        info.FileName = @"C:\Users\popov\Desktop\C#\ChildApp\bin\Debug\net10.0\ChildApp.exe";
+
+        Process process = new Process();
+        process.StartInfo = info;
+        process.Start();
+        process.WaitForExit();
+        var exitCode = process.ExitCode;
+        Console.WriteLine($"Дочірній процес завершився з кодом: {process.ExitCode}");
+    }
+}
